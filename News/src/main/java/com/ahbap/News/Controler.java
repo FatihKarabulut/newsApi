1a80091def456f7afdbcb3319a183acd28ac2419
FatihKarabulut <fatihbey908@gmail.com>
2025-01-18 21:25:19 +0300

    update

--- News/src/main/java/com/ahbap/News/Controler.java
+++ News/src/main/java/com/ahbap/News/Controler.java
@@ -44,5 +44,136 @@ public class Controler {
         }
         return new ArticleSearch();
     }
+    @CrossOrigin(origins = "*")
+    @GetMapping("/current")
+    public ArticleSearch current()
+    {
+        try {
 
+            return service.currentNews();
+        }
+        catch (Throwable ex)
+        {
+            log.error("Throwable Exception currentNews {} ",ex.getMessage());
+        }
+        return new ArticleSearch();
+    }
+    @CrossOrigin(origins = "*")
+    @GetMapping("/siyaset")
+    public ArticleSearch Siyaset() {
+
+            try {
+
+                return service.Siyaset();
+
+            }
+            catch (Throwable ex) {
+                log.error("Throwable Exception siyasetNews {} ", ex.getMessage());
+            }
+            return new ArticleSearch();
+
+    }
+    @CrossOrigin(origins = "*")
+    @GetMapping("/futbool")
+    public ArticleSearch futbool() {
+
+        try {
+
+            return service.futbool();
+
+        }
+        catch (Throwable ex) {
+            log.error("Throwable Exception futboolNews {} ", ex.getMessage());
+        }
+        return new ArticleSearch();
+
+    }
+    @CrossOrigin(origins = "*")
+    @GetMapping("/basketbool")
+    public ArticleSearch basketbool() {
+
+        try {
+
+            return service.basketbool();
+
+        }
+        catch (Throwable ex) {
+            log.error("Throwable Exception basketboolNews {} ", ex.getMessage());
+        }
+        return new ArticleSearch();
+
+    }
+    @CrossOrigin(origins = "*")
+    @GetMapping("/teknoloji")
+    public ArticleSearch teknoloji() {
+
+        try {
+
+            return service.teknoloji();
+
+        }
+        catch (Throwable ex) {
+            log.error("Throwable Exception teknolojiNews {} ", ex.getMessage());
+        }
+        return new ArticleSearch();
+
+    }
+    @CrossOrigin(origins = "*")
+    @GetMapping("/eğitim")
+    public ArticleSearch eğitim() {
+
+        try {
+
+            return service.eğitim();
+
+        }
+        catch (Throwable ex) {
+            log.error("Throwable Exception eğitimNews {} ", ex.getMessage());
+        }
+        return new ArticleSearch();
+
+    }
+    @CrossOrigin(origins = "*")
+    @GetMapping("/kultur")
+    public ArticleSearch kultur() {
+
+        try {
+
+            return service.kultur();
+
+        }
+        catch (Throwable ex) {
+            log.error("Throwable Exception kulturNews {} ", ex.getMessage());
+        }
+        return new ArticleSearch();
+
+    }
+    @CrossOrigin(origins = "*")
+    @GetMapping("/sağlık")
+    public ArticleSearch sağlık() {
+
+        try {
+
+            return service.sağlık();
+
+        }
+        catch (Throwable ex) {
+            log.error("Throwable Exception sağlıkNews {} ", ex.getMessage());
+        }
+        return new ArticleSearch();
+
+    }
+    @CrossOrigin(origins = "*")
+    @GetMapping("sondakika")
+    public ArticleSearch sonDakika()
+    {
+        try {
+            return service.SonDakika();
+        }
+        catch (Throwable ex)
+        {
+            log.error("SonDakikaNews Throwable {} ",ex.getMessage());
+        }
+        return new ArticleSearch();
+    }
 }
--- News/src/main/java/com/ahbap/News/ControlerService.java
+++ News/src/main/java/com/ahbap/News/ControlerService.java
@@ -14,51 +14,162 @@ import java.time.LocalDate;
 public class ControlerService {
     private static final Logger log = LoggerFactory.getLogger(ControlerService.class);
     public RestTemplate rest;
-    private static final String m_Url ="https://newsapi.org/v2/everything?q=%s&from=%d-%d-%d&sortBy=popularity&apiKey=652f7a9cea6e4975baadd200bf9799ee";
+    private static final String m_Url = "https://newsapi.org/v2/everything?q=%s&from=%d-%d-%d&sortBy=popularity&apiKey=652f7a9cea6e4975baadd200bf9799ee";
 
     // https://newsapi.org/v2/everything?q=iran&from=2024-11-19&sortBy=popularity&apiKey=652f7a9cea6e4975baadd200bf9799ee
     public ControlerService(RestTemplate rest) {
         this.rest = rest;
     }
 
-    public ArticleSearch newsSearch()
-    {
+    public ArticleSearch newsSearch() {
 
-      try {
-          var now = LocalDate.now();
-          var year = now.getYear();
-          var month = now.getMonthValue();
-          var day = now.getDayOfMonth() - 2;
-
-          var urls = m_Url.formatted("türkiye",year, month, day);
-
-          log.info("Url ---------------------- {}", urls);
-          return rest.getForObject(urls, ArticleSearch.class);
-      }
-      catch (Throwable ex)
-      {
-          log.error("Throwable Exception newsSearch {} ",ex.getMessage());
-      }
-        return new ArticleSearch();
-    }
-
-    public ArticleSearch newsCityh(String city)
-    {
         try {
             var now = LocalDate.now();
             var year = now.getYear();
             var month = now.getMonthValue();
             var day = now.getDayOfMonth() - 2;
-            var urls = m_Url.formatted(city,year, month, day);
-            log.info("url {}",urls);
-            return rest.getForObject(urls,ArticleSearch.class);
-        }
-        catch (Throwable ex)
-        {
-            log.error("Throwable Exception newsCity {} ",ex.getMessage());
+
+            var urls = m_Url.formatted("türkiye", year, month, day);
+
+            log.info("Url ---------------------- {}", urls);
+            return rest.getForObject(urls, ArticleSearch.class);
+        } catch (Throwable ex) {
+            log.error("Throwable Exception newsSearch {} ", ex.getMessage());
         }
         return new ArticleSearch();
     }
 
+    public ArticleSearch newsCityh(String city) {
+        try {
+            var now = LocalDate.now();
+            var year = now.getYear();
+            var month = now.getMonthValue();
+            var day = now.getDayOfMonth() - 2;
+            var urls = m_Url.formatted(city, year, month, day);
+            log.info("url {}", urls);
+            return rest.getForObject(urls, ArticleSearch.class);
+        } catch (Throwable ex) {
+            log.error("Throwable Exception newsCity {} ", ex.getMessage());
+        }
+        return new ArticleSearch();
+    }
 
+    public ArticleSearch currentNews() {
+        try {
+            var now = LocalDate.now();
+            var month = now.getMonthValue();
+            var day = now.getDayOfMonth() - 2;
+            var year = now.getYear();
+
+            var url = m_Url.formatted("Güncel", year, month, day);
+            log.info("current {}", url);
+            return rest.getForObject(url, ArticleSearch.class);
+
+        } catch (Throwable ex) {
+            log.error("Current Throwable exception {}",
+                    ex.getMessage());
+        }
+        return new ArticleSearch();
+    }
+
+    public ArticleSearch Siyaset() {
+        try {
+            var now = LocalDate.now();
+            var month = now.getMonthValue();
+            var day = now.getDayOfMonth() - 2;
+            var year = now.getYear();
+
+            var url = m_Url.formatted("Siyaset", year, month, day);
+            log.info("Siyaset {}",url);
+
+            return rest.getForObject(url, ArticleSearch.class);
+        } catch (Throwable ex) {
+            log.error("Spor Throwable Exception {}", ex.getMessage());
+        }
+        return new ArticleSearch();
+    }
+
+    public ArticleSearch futbool()
+    {
+        try {
+
+            return this.newsCityh("Futbol");
+        }
+        catch (Throwable ex)
+        {
+            log.error("futbool Throwable {}",ex.getMessage());
+        }
+        return new ArticleSearch();
+    }
+    public ArticleSearch basketbool()
+    {
+        try {
+
+            return this.newsCityh("Basketbol");
+        }
+        catch (Throwable ex)
+        {
+            log.error("basketbool Throwable {}",ex.getMessage());
+        }
+        return new ArticleSearch();
+    }
+    public ArticleSearch teknoloji()
+    {
+        try {
+
+            return this.newsCityh("Teknoloji");
+        }
+        catch (Throwable ex)
+        {
+            log.error("Teknoloji Throwable {}",ex.getMessage());
+        }
+        return new ArticleSearch();
+    }
+
+    public ArticleSearch sağlık()
+    {
+        try {
+
+            return this.newsCityh("Sağlık");
+        }
+        catch (Throwable ex)
+        {
+            log.error("Sağlık Throwable {}",ex.getMessage());
+        }
+        return new ArticleSearch();
+    }
+    public ArticleSearch eğitim()
+    {
+        try {
+
+            return this.newsCityh("Eğitim");
+        }
+        catch (Throwable ex)
+        {
+            log.error("eğitim Throwable {}",ex.getMessage());
+        }
+        return new ArticleSearch();
+    }
+    public ArticleSearch kultur()
+    {
+        try {
+
+            return this.newsCityh("Kültür ve Sanat");
+        }
+        catch (Throwable ex)
+        {
+            log.error("Kultur Throwable {}",ex.getMessage());
+        }
+        return new ArticleSearch();
+    }
+    public ArticleSearch SonDakika() {
+        try {
+            return this.newsCityh("Son Dakika");
+        }
+        catch (Throwable ex) {
+            log.error("Sondakika Throwable {}", ex.getMessage());
+        }
+        return new ArticleSearch();
+    }
 }
+
